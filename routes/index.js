af44315736bec934453c5746e6bae7e60576854a
Jason Lam <jaylam111@gmail.com>
2017-01-17 17:48:41 -0500

    finish link to username to show tweets

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,11 @@ router.get('/', function (req, res) {
   res.render( 'index', { tweets: tweets } );
 });
 
-router.get( '/users/:name', function(req, res){
-    
-} );
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var tweets = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets: tweets } );
+});
 
 
 // router.get('/stylesheets/style.css', function(req, res, next){
